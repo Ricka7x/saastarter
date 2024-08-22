5faf15620449ecdf7f76f0804d2ad9f517a1a793
Ricardo Ramirez <ricky.ramirez89@gmail.com>
2024-08-21 18:49:10 -0600

    still

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/docs',
+        destination: 'https://saastarter-docs.vercel.app',
+      },
+      {
+        source: '/docs/:path*',
+        destination: 'https://saastarter-docs.vercel.app/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
